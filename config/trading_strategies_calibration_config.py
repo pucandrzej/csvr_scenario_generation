5f2845002449ce082e480272d23ffce647c0a78c
riczi <ryszardrolik@gmail.com>
2026-08-13 22:33:36 +0200

    feature: update the median calibration config

--- config/trading_strategies_calibration_config.py
+++ config/trading_strategies_calibration_config.py
@@ -15,12 +15,12 @@ bands_grid_config = {
 
 median_grid_config = {
     'scp': [np.nan],
-    'p_list': [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3],
+    'p_list': [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4],
     'lambda_list': [
         0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7,  # one every 0.05 up to 0.7
         0.8, 0.9, 1.0,  # then one every 0.1 up to 1
         1.25, 1.5,  # two 0.25 apart
-        2, 3, 4, 5  # four 1 apart
+        2, 3, 4, 5, 6  # five 1 apart
         ],
     'trust_threshold_method': [
         "3sigma",
